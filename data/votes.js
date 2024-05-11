ac876706b6046c7306d1f7ff798a760309ce896f
arnaudsm <arnaud.desaintmeloir@gmail.com>
2024-05-11 23:56:10 +0200

    wip

--- data/votes.js
+++ data/votes.js
@@ -5,7 +5,7 @@ import { deputes } from "./lists.js";
 
 export async function* iterateVotes() {
   const reader = readline.createInterface({
-    input: fs.createReadStream("vote_stats.json.gz").pipe(zlib.createGunzip()),
+    input: fs.createReadStream("votes.json.gz").pipe(zlib.createGunzip()),
   });
   for await (const line of reader) {
     const vote = JSON.parse(line);
